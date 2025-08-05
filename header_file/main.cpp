de70cb077a270888f63efb5feed2f8495f8b61f9
root <abelbenardm@gmail.com>
2025-08-05 07:23:33 +0000

    added header files example usage

---
+++ header_file/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "math_lib.h"
+using namespace std;
+
+int main()
+{
+    cout << add(5, 4) << endl;
+    cout << sub(3, 9) << endl;
+    return 0;
+}
---
+++ header_file/math_lib.h
@@ -0,0 +1,7 @@
+#ifndef MATH_LIB_H
+#define MATH_LIB_H
+
+int add(int x, int y);
+int sub(int x, int y);
+
+#endif
